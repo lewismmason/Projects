efa81cb3f8fabc7cd5a78c92df499301b36d6614
Lewis <lewis.montgomery.mason@gmail.com>
2025-03-05 20:13:34 -0800

    The game is now playable and both players can earn points! But there are no visuals or animations. Ill add some small ones next such as showing what the opponent chose.

--- MP1/game-client/client.gd
+++ MP1/game-client/client.gd
@@ -6,6 +6,15 @@ var multiplayer_peer : ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 
 var _connected_peer_ids = []
 
+enum RPS {NONE, ROCK, PAPER, SCISSORS}
+var selected = RPS.NONE
+
+
+enum PLAYER {PLAYER, OPPONENT, NONE}
+var winning = PLAYER.NONE
+var player_score = 0
+var opponent_score = 0
+
 
 func _ready():
 	multiplayer.server_disconnected.connect(_on_server_disconnected)
@@ -36,6 +45,7 @@ func _on_disconnect_button_up():
 func _on_server_disconnected() -> void:
 	multiplayer_peer.close()
 	update_connection_button_status()
+	reset_game()
 	print("Connection to server lost.")
 
 
@@ -56,13 +66,14 @@ func _on_connection_failed() -> void:
 
 func update_connection_button_status() -> void:
 	$ConnectionTimeOut.stop() # We stop the timer here...
-	
+
 	if multiplayer_peer.get_connection_status() == multiplayer_peer.ConnectionStatus.CONNECTION_CONNECTED:
 		$CanvasLayer/VBoxContainer/Connect.disabled = true
 		$CanvasLayer/VBoxContainer/Connect.modulate = Color(0.5,0.5,0.5)
 		$CanvasLayer/VBoxContainer/Disconnect.disabled = false
 		$CanvasLayer/VBoxContainer/Disconnect.modulate = Color(1,1,1)
 		$CanvasLayer/Gameplay.visible = true
+		$CanvasLayer/Score.visible = true
 		$CanvasLayer/VBoxContainer/ID.text = str(multiplayer_peer.get_unique_id())
 		
 	elif multiplayer_peer.get_connection_status() == multiplayer_peer.ConnectionStatus.CONNECTION_DISCONNECTED:
@@ -72,7 +83,8 @@ func update_connection_button_status() -> void:
 		$CanvasLayer/VBoxContainer/Disconnect.modulate = Color(0.5,0.5,0.5)
 		$CanvasLayer/VBoxContainer/ID.text = "Disconnected."
 		$CanvasLayer/Gameplay.visible = false
-		
+		$CanvasLayer/Score.visible = false
+
 	elif multiplayer_peer.get_connection_status() == multiplayer_peer.ConnectionStatus.CONNECTION_CONNECTING:
 		$CanvasLayer/VBoxContainer/Connect.disabled = true
 		$CanvasLayer/VBoxContainer/Connect.modulate = Color(0.5,0.5,0.5)
@@ -80,12 +92,102 @@ func update_connection_button_status() -> void:
 		$CanvasLayer/VBoxContainer/Disconnect.modulate = Color(0.5,0.5,0.5)
 		$CanvasLayer/VBoxContainer/ID.text = "Connecting..."
 		$CanvasLayer/Gameplay.visible = false
+		$CanvasLayer/Score.visible = false
 
+
+func reset_game() -> void:
+	#reset score, reset turn, etc
+	reset_turn()
+
+
+func reset_turn() -> void:
+	self._select(RPS.NONE)
+ 
+
+func reset_score() -> void:
+	self.opponent_score = 0
+	self.player_score = 0
+	self._update_score_visual()
+
+
+func _select(s : int) -> void:
+	self.selected = s
+	$CanvasLayer/Gameplay/Player/Rock/Rock.disabled = true
+	$CanvasLayer/Gameplay/Player/Paper/Paper.disabled = true
+	$CanvasLayer/Gameplay/Player/Scissors/Scissors.disabled = true
+
+	match s:
+		RPS.ROCK:
+			$CanvasLayer/Gameplay/Player/Rock.self_modulate = Color(0.8,1,0.8)
+			$CanvasLayer/Gameplay/Player/Paper.modulate = Color(1,1,1,0.3)
+			$CanvasLayer/Gameplay/Player/Scissors.modulate = Color(1,1,1,0.3)
+
+		RPS.PAPER:
+			$CanvasLayer/Gameplay/Player/Rock.modulate = Color(1,1,1,0.3)
+			$CanvasLayer/Gameplay/Player/Paper.self_modulate = Color(0.8,1,0.8)
+			$CanvasLayer/Gameplay/Player/Scissors.modulate = Color(1,1,1,0.3)
+
+		RPS.SCISSORS:
+			$CanvasLayer/Gameplay/Player/Rock.modulate = Color(1,1,1,0.3)
+			$CanvasLayer/Gameplay/Player/Paper.modulate = Color(1,1,1,0.3)
+			$CanvasLayer/Gameplay/Player/Scissors.self_modulate = Color(0.8,1,0.8)
+
+		RPS.NONE:
+			$CanvasLayer/Gameplay/Player/Rock/Rock.disabled = false
+			$CanvasLayer/Gameplay/Player/Rock.modulate = Color(1,1,1,1)
+			$CanvasLayer/Gameplay/Player/Rock.self_modulate = Color(1,1,1,1)
+			$CanvasLayer/Gameplay/Player/Paper/Paper.disabled = false
+			$CanvasLayer/Gameplay/Player/Paper.modulate = Color(1,1,1,1)
+			$CanvasLayer/Gameplay/Player/Paper.self_modulate = Color(1,1,1,1)
+			$CanvasLayer/Gameplay/Player/Scissors/Scissors.disabled = false
+			$CanvasLayer/Gameplay/Player/Scissors.modulate = Color(1,1,1,1)
+			$CanvasLayer/Gameplay/Player/Scissors.self_modulate = Color(1,1,1,1)
+
+	if multiplayer_peer.get_connection_status() == multiplayer_peer.ConnectionStatus.CONNECTION_CONNECTED:
+		rpc("move_selected", multiplayer.get_unique_id(), s)
+
+
+func _on_rock_button_up(): self._select(RPS.ROCK)
+
+
+func _on_paper_button_up(): self._select(RPS.PAPER)
+
+
+func _on_scissors_button_up(): self._select(RPS.SCISSORS)
+
+
+func _update_score_visual() -> void:
+	$CanvasLayer/Score/Score/Opponent.text = str(opponent_score)
+	$CanvasLayer/Score/Score/Player.text = str(player_score)
 
 #region RPC
-@rpc 
+
+@rpc("authority", "call_remote")
 func sync_player_list(connected_peer_ids) -> void:
 	self._connected_peer_ids = connected_peer_ids
-	#multiplayer_peer.get_unique_id()
 	print("Currently connected players: " + str(_connected_peer_ids))
+
+
+@rpc("any_peer", "call_remote")
+func move_selected(peer_id : int, peer_move : int) -> void:
+	print('Function move_selected ran on client, nothing occurs.')
+
+
+@rpc("authority", "call_local")
+func peer_winner(peer_id : int) -> void:
+	if peer_id == multiplayer.get_unique_id():
+		print("Player won the round.")
+		player_score += 1 
+	elif _connected_peer_ids.has(peer_id): # sneaky way to check if opponent won, depends on previous if check.
+		print("Opponent won the round.")
+		opponent_score += 1
+	elif peer_id == -1:
+		print("Tie. Both players are awarded points because thats more fun!")
+		player_score += 1 
+		opponent_score += 1
+	else:
+		printerr("Peer winner has an invalid peer_id of " + str(peer_id) + ".") # Should never reach here?
+
+	self._update_score_visual()
+	self.reset_turn()
 #endregion
--- MP1/game-server/server.gd
+++ MP1/game-server/server.gd
@@ -5,7 +5,14 @@ const MAX_CLIENTS = 2
 
 var multiplayer_peer : ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 
-var connected_peer_ids = []
+var connected_peer_ids : Array = []
+
+# I don't like that I have to copy paste stuff like this enum with this method of dedicated servers.
+# A potential fix is to not separate server and client, making clients able to run in "full mode" (client) or a low-cost mode (server)
+# where the low cost mode does not run any UI, etc, and only holds game state/moves. TODO after I finish the game with this yuck method for practice...
+enum RPS {NONE, ROCK, PAPER, SCISSORS} 
+var connected_peer_moves : Array = [] # Array with moves corresponding to users in the connected_peer_ids
+
 
 func _ready():
 	var err = multiplayer_peer.create_server(PORT,MAX_CLIENTS)
@@ -16,7 +23,6 @@ func _ready():
 
 	multiplayer.multiplayer_peer = multiplayer_peer
 
-	#multiplayer_peer.set_bind_ip("127.0.0.1")
 	multiplayer_peer.peer_connected.connect(_on_peer_connected)
 	multiplayer_peer.peer_disconnected.connect(_on_peer_disconnected)
 	print("Server created.")
@@ -33,6 +39,8 @@ func _add_player(new_peer_id : int) -> void:
 	connected_peer_ids.append(new_peer_id)
 	print('Connected players: ' + str(connected_peer_ids))
 	self._update_list()
+	connected_peer_moves.append(RPS.NONE)
+	$"CanvasLayer/Client Moves".text = str(connected_peer_moves)
 	rpc('sync_player_list', connected_peer_ids) # TODO read rpc doc and make sure its safe
 
 
@@ -46,6 +54,8 @@ func _remove_player(peer_id : int) -> void:
 	
 	if peer_id_idx != -1:
 		connected_peer_ids.remove_at(peer_id_idx)
+		connected_peer_moves.remove_at(peer_id_idx)
+		$"CanvasLayer/Client Moves".text = str(connected_peer_moves)
 	
 	print('Player ' + str(peer_id) + ' disconnected.')
 	self._update_list()
@@ -57,5 +67,55 @@ func _update_list() -> void:
 
 
 # Server must have rpc functions, called on client
-@rpc func sync_player_list(connected_peer_ids) -> void:
-	pass
+#region RPC functions
+
+@rpc("authority", "call_remote")
+func sync_player_list(connected_peer_ids) -> void:
+	print("Function sync_player_list ran on server, nothing occurs.")
+
+
+@rpc("any_peer", "call_remote")
+func move_selected(peer_id : int, peer_move : int) -> void:
+	var idx = connected_peer_ids.find(peer_id)
+
+	if idx == -1: 
+		printerr("Invalid peer ID called for move_selected.")
+		return
+	
+	if connected_peer_moves.size() < idx:
+		printerr("Peer moves array size less than the indix changed.")
+		return
+
+	if not RPS.values().has(peer_move):
+		printerr("Invalid peer move.")
+		return
+
+	connected_peer_moves[idx] = peer_move
+	$"CanvasLayer/Client Moves".text = str(connected_peer_moves)
+
+	# If both players have selected a move, see who wins
+	if connected_peer_moves.size() != 2: return # only one client connected
+	if connected_peer_moves[0] == RPS.NONE or connected_peer_moves[1] == RPS.NONE: return # only one client has done their move
+	
+	print("Both clients selected a move, determining winner...")
+	
+	var d = connected_peer_moves[0] - connected_peer_moves[1] # sneaky way to determine winner based on the enum!
+	
+	if d == 0:
+		print("Tie, both players are awarded points.")
+		rpc("peer_winner",-1)
+	elif d == 1 or d == -2: 
+		print("Peer " + str(connected_peer_ids[0]) + " won the round!")
+		rpc("peer_winner",connected_peer_ids[0])
+	elif d == -1 or d == 2:
+		print("Peer " + str(connected_peer_ids[1]) + " won the round!")
+		rpc("peer_winner",connected_peer_ids[1])
+		
+
+
+@rpc("authority", "call_local")
+func peer_winner(peer_id : int) -> void:
+	for i in range(0,connected_peer_moves.size()):
+		connected_peer_moves[i] = RPS.NONE # is this a race condition? IE if the client disconnects at exactly this point will this crash?
+
+#endregion
